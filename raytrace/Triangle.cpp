7ec7db8c20f5d2ab5fe3b68c34c7d6bab3e145f3
wuhaozhe <506903514@qq.com>
2017-04-05 12:20:47 +0800

    not finished triangle

--- raytrace/Triangle.cpp
+++ raytrace/Triangle.cpp
@@ -4,9 +4,28 @@
 
 Triangle::Triangle()
 {
+	init();
 }
 
 
 Triangle::~Triangle()
 {
+}
+
+void Triangle::init()
+{
+	object_type = Object_type::Triangle;
+}
+
+Triangle::Triangle(vector3<double> vertex1, vector3<double> vertex2, vector3<double> vertex3)
+{
+	init();
+	vertex[0] = vertex1;
+	vertex[1] = vertex2;
+	vertex[2] = vertex3;
+}
+
+bool Triangle::intersect(Ray input_ray, vector3<double> &intersect_point)
+{
+
 }
--- raytrace/Triangle.h
+++ raytrace/Triangle.h
@@ -2,8 +2,14 @@
 #include "Object.h"
 class Triangle: public Object
 {
+private:
+	void init();
 public:
+	vector3<double> vertex[3];           //三角形的三个顶点
 	Triangle();
+	Triangle(vector3<double> vertex1, vector3<double> vertex2, vector3<double> vertex3);
+	bool intersect(Ray input_ray, vector3<double> &intersect_point);
+	Color get_color(vector3<double> target_pos, vector3<double> view_direction, Light light);
 	~Triangle();
 };
 
